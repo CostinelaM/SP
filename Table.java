2b1ce74a4fcd511cd8f6e51bbef31277ce68d9e9
CostinelaM <37276500+CostinelaM@users.noreply.github.com>
2018-10-15 13:09:16 +0300

    Update Table.java

--- Table.java
+++ Table.java
@@ -1,13 +1,7 @@
-
 public class Table {
-	private String name;
-	public Table (String name) {
-		this.name = name;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
+	String name;
+	
+	public Table(String name){
 		this.name = name;
 	}
 }
